dfea60f0996d9378cf60bc491642d64d0cc5be0b
riddell999 <dell.smith999@gmail.com>
2025-02-24 19:37:43 -0600

    fadf

--- stats.py
+++ stats.py
@@ -11,32 +11,3 @@ def count_words():
         word_count = len(words)
     return word_count           
 
-def character_count():
-    char_dict = {}
-    with open(FRANKENSTEIN_FILE) as f:
-        frankenstein_text = f.read()
-    lowered_text = frankenstein_text.lower()
-    for character in lowered_text:
-        if character not in char_dict:
-            char_dict[character] = 1
-        else:
-            char_dict[character] += 1 
-    return char_dict
-
-def report():
-    print(f"--- Begin report of {FRANKENSTEIN_FILE} ---")
-    number_words = f"{count_words()} words found in the document"
-    print(number_words)
-    char_dict = character_count()
-    char_list = []
-    for char, count in char_dict.items():
-        if char.isalpha():
-            char_list.append({"char":char,"num": count})
-    def sort_on(char_list):
-        return char_list["num"]
-    char_list.sort(reverse=True, key=sort_on)
-    for character in char_list:
-        print(f"The '{character["char"]}' character was found {character["num"]} times")    
-
-
-report()
